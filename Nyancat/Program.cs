e6d27cd3a1a0d9ca4836bfdeab136981d113c872
Nick Van Dyck <vandyck.nick@outlook.com>
2018-06-12 13:41:43 +0100

    remove unused using statements

--- Nyancat/Program.cs
+++ Nyancat/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Reflection;
-using System.Threading;
+﻿using System.Reflection;
 using McMaster.Extensions.CommandLineUtils;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;

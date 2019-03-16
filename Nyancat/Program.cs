54b08b509bec96bea265e1111b7c3169e787fa32
Nick Van Dyck <vandyck.nick@outlook.com>
2019-03-16 16:52:41 +0100

    Use mono.options to take a dependency on a smaller dll

--- Nyancat/Program.cs
+++ Nyancat/Program.cs
@@ -1,54 +1,83 @@
-﻿using System.Reflection;
-using McMaster.Extensions.CommandLineUtils;
+﻿using System;
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Mono.Options;
 using Nyancat.Graphics;
 using Nyancat.Scenes;
 
 namespace Nyancat
 {
-    [Command(Name = "nyancat", FullName = "nyancat", Description = "Terminal nyancat runner")]
-    [VersionOptionFromMember(MemberName = nameof(GetVersion))]
     class Program
     {
-        [Option(Description = "Show the introduction / about information at startup", ShortName = "i")]
-        public bool Intro { get; set; } = false;
-
-        [Option(Description = "Do not display the timer", ShortName = "n")]
-        public bool NoCounter { get; set; } = false;
-
-        [Option(Description = "Do not set the titlebar text", ShortName = "t")]
-        public bool NoTitle { get; set; } = false;
-
-        [Option(Description = "Display the requested number of frames, then quit", ShortName = "f")]
-        public int Frames { get; set; } = int.MaxValue;
-
-        // [Option(Description = "Play sound during animation (only works on windows)", ShortName = "s")]
-        public bool Sound { get; set; } = false;
-
-        public void OnExecute()
+        public static int Main(string[] args)
         {
-            var host = new ConsoleGraphicsHostBuilder()
-                .AddScene<IntroScene>(isStartup: Intro)
-                .AddScene<NyancatScene>(isStartup: !Intro)
-                .ConfigureServices((context, services) =>
+            bool showIntro = false;
+            bool showHelp = false;
+            bool noCounter = false;
+            bool noTitle = false;
+            bool sound = false;
+            int frames = int.MaxValue;
+
+            var options = new OptionSet
+            {
+                { "i|intro", "Show the introduction / about information at startup", i => showIntro = true },
+                { "n|no-counter", "Do not display the timer", n => noCounter = true },
+                { "t|no-title", "Do not set the titlebar text", t => noTitle = true },
+                { "f|frames", "Display the requested number of frames, then quit", (int f) => frames = f },
+                { "?|h|help", "Show help information", h => showHelp = h != null },
+            };
+
+            try
+            {
+                var extra = options.Parse(args);
+
+                if (showHelp)
                 {
-                    services.Configure<NyancatSceneOptions>(options =>
+                    Console.WriteLine($"{GetName()}: {GetVersion()}");
+                    Console.WriteLine();
+                    Console.WriteLine("Terminal nyancat runner");
+                    Console.WriteLine();
+                    Console.WriteLine($"Usage: {GetName()} [options]");
+                    Console.WriteLine();
+                    Console.WriteLine("Options");
+                    Console.WriteLine();
+                    options.WriteOptionDescriptions(Console.Out);
+                    return 0;
+                }
+
+                var host = new ConsoleGraphicsHostBuilder()
+                    .AddScene<IntroScene>(isStartup: showIntro)
+                    .AddScene<NyancatScene>(isStartup: !showIntro)
+                    .ConfigureServices((context, services) =>
                     {
-                        options.ShowTitle = !NoTitle;
-                        options.ShowCounter = !NoCounter;
-                        options.Frames = Frames;
-                        options.Sound = Sound;
-                    });
-                })
-                .Build();
+                        services.Configure<NyancatSceneOptions>(sceneOptions =>
+                        {
+                            sceneOptions.ShowTitle = !noTitle;
+                            sceneOptions.ShowCounter = !noCounter;
+                            sceneOptions.Frames = frames;
+                            sceneOptions.Sound = sound;
+                        });
+                    })
+                    .Build();
 
-            host.Run();
+                host.Run();
+
+                return 0;
+            }
+            catch (OptionException e)
+            {
+                Console.WriteLine($"{GetName()}: {GetVersion()}");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `nyancat --help' for more information.");
+                return 1;
+            }
         }
-
-        public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
 
         private static string GetVersion()
             => typeof(Program).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+
+        private static string GetName()
+            => typeof(Program).Assembly.GetCustomAttribute<AssemblyProductAttribute>().Product;
     }
 }

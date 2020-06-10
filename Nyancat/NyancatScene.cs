aa8e4418901c4b60c1c3303a2377607c3a4f5742
Nick Van Dyck <vandyck.nick@outlook.com>
2020-06-10 20:27:45 +0200

    chore: add nyancat animation into scene

---
+++ Nyancat/NyancatScene.cs
@@ -0,0 +1,260 @@
+using System;
+using System.Collections.Generic;
+
+namespace Nyancat
+{
+    public struct NyancatScene : IScene
+    {
+        private const string RAINBOW_TAIL = ",,>>&&&+++###==;;;,,";
+
+        private int _frames;
+        private int _showCounter;
+        private int _width, _height;
+        private int _minCol, _maxCol, _minRow, _maxRow;
+        private int _fc;
+        private long _startTime;
+
+        private Dictionary<char, string> _colors;
+
+        public NyancatScene(int frames = int.MaxValue, int showCounter = 1)
+        {
+            _frames = frames;
+            _showCounter = showCounter;
+            _width = 80;
+            _height = 24;
+
+            _minCol = _maxCol = _minRow = _maxRow = 0;
+
+            _fc = int.MinValue;
+            _startTime = long.MinValue;
+
+            _colors = GetColors();
+
+            CalculateRowsAndCols(width: _width, height: _height);
+        }
+
+        private void CalculateRowsAndCols(int width, int height)
+        {
+            _minCol = (NyancatFrames.FRAME_WIDTH - width / 2) / 2;
+            _maxCol = (NyancatFrames.FRAME_WIDTH + width / 2) / 2;
+
+            _minRow = (NyancatFrames.FRAME_HEIGHT - (height - _showCounter)) / 2;
+            _maxRow = (NyancatFrames.FRAME_HEIGHT + (height - _showCounter)) / 2;
+        }
+
+        private void CheckWindowSize(int width, int height)
+        {
+            if (_height != height || _width != width)
+            {
+                _height = height;
+                _width = width;
+
+                CalculateRowsAndCols(width: _width, height: _height);
+            }
+        }
+
+        public bool Update(int width, int height)
+        {
+            if (_frames == 0) return false;
+
+            CheckWindowSize(width: width, height: height);
+
+            _fc = _fc == int.MinValue ? 0 : _fc + 1;
+
+            if (_fc > NyancatFrames.TOTAL_FRAMES)
+            {
+                _fc = 0;
+            }
+
+            if (_startTime == long.MinValue)
+            {
+                _startTime = Environment.TickCount64;
+            }
+
+            if (_frames != int.MaxValue) _frames--;
+
+            return true;
+        }
+
+        public void Render(ref ConsoleGraphics console)
+        {
+            var frame = NyancatFrames.GetFrame(_fc);
+            var lastPixel = char.MinValue;
+
+            console.ResetCursor();
+
+            for (var row = _minRow; row < _maxRow; ++row)
+            {
+                for (var col = _minCol; col < _maxCol; ++col)
+                {
+                    char pixel;
+                    if (row > 23 && row < 43 && col < 0)
+                    {
+
+                        int mod_x = ((-col + 2) % 16) / 8;
+                        if ((_fc / 2) % 2 == 0)
+                        {
+                            mod_x = 1 - mod_x;
+                        }
+
+                        var index = mod_x + row - 23;
+
+                        pixel = ',';
+                        if (index < RAINBOW_TAIL.Length)
+                        {
+                            pixel = RAINBOW_TAIL[index];
+                        }
+                    }
+                    else if (row < 0 || col < 0 || row >= NyancatFrames.FRAME_HEIGHT || col >= NyancatFrames.FRAME_WIDTH)
+                    {
+                        pixel = ',';
+                    }
+                    else
+                    {
+                        var start = row * NyancatFrames.FRAME_WIDTH + (row * 1);
+
+                        start += col;
+
+                        pixel = frame.Slice(start, 1)[0];
+                    }
+
+                    if (ConsoleColorSupport.Level == ColorSupportLevel.None)
+                    {
+                        lastPixel = pixel;
+                        console.Write(_colors[pixel]);
+                        console.Write(_colors[pixel]);
+                    }
+                    else if (pixel != lastPixel && _colors.ContainsKey(pixel))
+                    {
+                        lastPixel = pixel;
+                        console.Write(_colors[pixel]);
+                        console.Write(' ');
+                        console.Write(' ');
+                    }
+                    else
+                    {
+                        console.Write(' ');
+                        console.Write(' ');
+                    }
+                }
+
+                console.WriteLine();
+            }
+
+            if (_showCounter == 1)
+            {
+                var totalTime = (Environment.TickCount64 - _startTime) / 1000;
+                var message = string.Concat("You have nyaned for ", totalTime, " seconds!");
+                var spacesLength = (_width - message.Length) / 2;
+
+                if (spacesLength > 0)
+                {
+                    Span<char> spaces = stackalloc char[spacesLength];
+
+                    for (var i = 0; i < spacesLength; i++)
+                    {
+                        spaces[i] = ConsoleColorSupport.Level == ColorSupportLevel.None ? ',' : ' ';
+                    }
+
+                    console.Write(spaces);
+                    console.ColorBrightWhite();
+                    console.Write(message);
+                    console.Write(spaces);
+                }
+            }
+
+            console.WriteLine();
+        }
+
+        private static Dictionary<char, string> GetColors()
+        {
+            if (ConsoleColorSupport.Level.HasFlag(ColorSupportLevel.TrueColor))
+            {
+                return new Dictionary<char, string>
+                {
+                    { ','  , "\x1b[48;2;0;0;95m" },         /* Blue background */
+                    { '.'  , "\x1b[48;2;255;255;255m" },    /* White stars */
+                    { '\'' , "\x1b[48;2;0;0;0m" },          /* Black border */
+                    { '@'  , "\x1b[48;2;255;255;215m" },    /* Tan poptart */
+                    { '$'  , "\x1b[48;2;255;192;203m" },    /* Pink poptart */
+                    { '-'  , "\x1b[48;2;215;0;135m" },      /* Red poptart */
+                    { '>'  , "\x1b[48;2;255;0;0m" },        /* Red rainbow */
+                    { '&'  , "\x1b[48;2;255;165;0m" },      /* Orange rainbow */
+                    { '+'  , "\x1b[48;2;255;255;0m" },      /* Yellow Rainbow */
+                    { '#'  , "\x1b[48;2;135;255;0m" },      /* Green rainbow */
+                    { '='  , "\x1b[48;2;0;135;255m" },      /* Light blue rainbow */
+                    { ';'  , "\x1b[48;2;0;0;175m" },        /* Dark blue rainbow */
+                    { '*'  , "\x1b[48;2;88;88;88m" },       /* Gray cat face */
+                    { '%'  , "\x1b[48;2;215;135;175m" },    /* Pink cheeks */
+                };
+            }
+            else if (ConsoleColorSupport.Level.HasFlag(ColorSupportLevel.Ansi256))
+            {
+                return new Dictionary<char, string>
+                {
+                    { ','  , "\x1b[48;5;17m" },  /* Blue background */
+                    { '.'  , "\x1b[48;5;231m" }, /* White stars */
+                    { '\'' , "\x1b[48;5;16m" },  /* Black border */
+                    { '@'  , "\x1b[48;5;230m" }, /* Tan poptart */
+                    { '$'  , "\x1b[48;5;175m" }, /* Pink poptart */
+                    { '-'  , "\x1b[48;5;162m" }, /* Red poptart */
+                    { '>'  , "\x1b[48;5;196m" }, /* Red rainbow */
+                    { '&'  , "\x1b[48;5;214m" }, /* Orange rainbow */
+                    { '+'  , "\x1b[48;5;226m" }, /* Yellow Rainbow */
+                    { '#'  , "\x1b[48;5;118m" }, /* Green rainbow */
+                    { '='  , "\x1b[48;5;33m" },  /* Light blue rainbow */
+                    { ';'  , "\x1b[48;5;19m" },  /* Dark blue rainbow */
+                    { '*'  , "\x1b[48;5;240m" }, /* Gray cat face */
+                    { '%'  , "\x1b[48;5;175m" }, /* Pink cheeks */
+                };
+            }
+            else if (ConsoleColorSupport.Level.HasFlag(ColorSupportLevel.Basic))
+            {
+                return new Dictionary<char, string>
+                {
+                    { ',' , "\x1b[104m" }, /* Blue background */
+                    { '.' , "\x1b[107m" }, /* White stars */
+                    { '\'' , "\x1b[40m" }, /* Black border */
+                    { '@' , "\x1b[47m" },  /* Tan poptart */
+                    { '$' , "\x1b[105m" }, /* Pink poptart */
+                    { '-' , "\x1b[101m" }, /* Red poptart */
+                    { '>' , "\x1b[101m" }, /* Red rainbow */
+                    { '&' , "\x1b[43m" },  /* Orange rainbow */
+                    { '+' , "\x1b[103m" }, /* Yellow Rainbow */
+                    { '#' , "\x1b[102m" }, /* Green rainbow */
+                    { '=' , "\x1b[104m" }, /* Light blue rainbow */
+                    { ';' , "\x1b[44m" },  /* Dark blue rainbow */
+                    { '*' , "\x1b[100m" }, /* Gray cat face */
+                    { '%' , "\x1b[105m" }, /* Pink cheeks */
+                };
+            }
+            else
+            {
+                return new Dictionary<char, string>
+                {
+                    { ','  , "," }, /* Blue background */
+                    { '.'  , "." }, /* White stars */
+                    { '\'' , "'" }, /* Black border */
+                    { '@'  , "@" }, /* Tan poptart */
+                    { '$'  , "$" }, /* Pink poptart */
+                    { '-'  , "-" }, /* Red poptart */
+                    { '>'  , ">" }, /* Red rainbow */
+                    { '&'  , "&" }, /* Orange rainbow */
+                    { '+'  , "+" }, /* Yellow Rainbow */
+                    { '#'  , "#" }, /* Green rainbow */
+                    { '='  , "=" }, /* Light blue rainbow */
+                    { ';'  , ";" }, /* Dark blue rainbow */
+                    { '*'  , "*" }, /* Gray cat face */
+                    { '%'  , "%" }, /* Pink cheeks */
+                };
+            }
+        }
+
+    }
+
+    interface IScene
+    {
+        bool Update(int width, int height);
+        void Render(ref ConsoleGraphics console);
+    }
+}
--- Nyancat/Program.cs
+++ Nyancat/Program.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Mono.Options;
@@ -10,13 +9,6 @@ namespace Nyancat
     public class Program
     {
         private const string RAINBOW_TAIL = ",,>>&&&+++###==;;;,,";
-
-        private const string HIDE_CURSOR = "\x1b[?25l";
-        private const string RESET_CURSOR = "\x1b[H";
-        private const string SHOW_CURSOR = "\x1b[?25h";
-        private const string CLEAR_SCREEN = "\x1b[2J";
-        private const string RESET_ALL_ATTRIBUTES = "\x1b[0m";
-
         private static readonly ManualResetEvent _shutdownBlock = new ManualResetEvent(false);
         public static int Main(string[] args)
         {
@@ -37,8 +29,6 @@ namespace Nyancat
                 { "v|version", "Show version information", _ => showVersion = true },
                 { "?|h|help", "Show help information", _ => showHelp = true },
             };
-
-            var colors = GetColors();
 
             try
             {
@@ -75,7 +65,43 @@ namespace Nyancat
                     running = false;
                 };
 
+                var console = new ConsoleGraphics(buffered: true);
+
+                if (showTitle) console.Title = "Nyanyanyanyanyanyanya...";
+
+                console
+                    .HideCursor()
+                    .ResetCursor()
+                    .Flush();
+
+                var defaultSleep = GetDefaultSleep();
+                var startTime = Environment.TickCount64;
+                var renderTime = startTime;
+
                 if (showIntro) Console.WriteLine("TODO: implement show intro");
+                var scene = new NyancatScene(frames: frames, showCounter: showCounter);
+
+                while (running)
+                {
+                    if (!scene.Update(console.Width, console.Height))
+                    {
+                        running = false;
+                    }
+
+                    scene.Render(ref console);
+
+                    console.Flush();
+
+                    var elapsed = Environment.TickCount64 - renderTime;
+                    var sleep = (defaultSleep * 2) - elapsed;
+                    Thread.Sleep((int)Math.Clamp(sleep, 0L, defaultSleep));
+                    renderTime = Environment.TickCount64;
+                }
+
+                console.Dispose();
+                _shutdownBlock.Set();
+
+                return 0;
             }
             catch (OptionException e)
             {
@@ -85,249 +111,22 @@ namespace Nyancat
                 Console.WriteLine("Try `nyancat --help' for more information.");
                 return 1;
             }
-
-            int height = 0;
-            int width = 0;
-            int minCol, maxCol, minRow, maxRow;
-
-            void CalculateRowsAndCols()
-            {
-                minCol = (NyancatFrames.FRAME_WIDTH - Console.WindowWidth / 2) / 2;
-                maxCol = (NyancatFrames.FRAME_WIDTH + Console.WindowWidth / 2) / 2;
-
-                minRow = (NyancatFrames.FRAME_HEIGHT - (Console.WindowHeight - showCounter)) / 2;
-                maxRow = (NyancatFrames.FRAME_HEIGHT + (Console.WindowHeight - showCounter)) / 2;
-            }
-
-            void CheckWindowSize()
-            {
-                if (height != Console.WindowHeight || width != Console.WindowWidth)
-                {
-                    height = Console.WindowHeight;
-                    width = Console.WindowWidth;
-
-                    CalculateRowsAndCols();
-                }
-            }
-
-            CalculateRowsAndCols();
-            var console = new ConsoleGraphics(buffered: true);
-
-            if (showTitle) console.Title = "Nyanyanyanyanyanyanya...";
-
-            console
-                .HideCursor()
-                .ResetCursor()
-                .Flush();
-
-            var defaultSleep = GetDefaultSleep();
-            var elapsed = new Stopwatch();
-            var watch = new Stopwatch();
-            watch.Start();
-            elapsed.Start();
-
-            while (running)
-            {
-                for (var fc = 0; fc <= NyancatFrames.TOTAL_FRAMES; fc++)
-                {
-                    CheckWindowSize();
-
-                    if (!running) break;
-
-                    console.ResetCursor();
-
-                    var frame = NyancatFrames.GetFrame(fc);
-                    char lastPixel = char.MinValue;
-
-                    for (var row = minRow; row < maxRow; ++row)
-                    {
-                        for (var col = minCol; col < maxCol; ++col)
-                        {
-                            char pixel;
-                            if (row > 23 && row < 43 && col < 0)
-                            {
-
-                                int mod_x = ((-col + 2) % 16) / 8;
-                                if ((fc / 2) % 2 == 0)
-                                {
-                                    mod_x = 1 - mod_x;
-                                }
-
-                                var index = mod_x + row - 23;
-
-                                pixel = ',';
-                                if (index < RAINBOW_TAIL.Length)
-                                {
-                                    pixel = RAINBOW_TAIL[index];
-                                }
-                            }
-                            else if (row < 0 || col < 0 || row >= NyancatFrames.FRAME_HEIGHT || col >= NyancatFrames.FRAME_WIDTH)
-                            {
-                                pixel = ',';
-                            }
-                            else
-                            {
-                                var start = row * NyancatFrames.FRAME_WIDTH + (row * 1);
-
-                                start += col;
-
-                                pixel = frame.Slice(start, 1)[0];
-                            }
-
-                            if (ConsoleColorSupport.Level == ColorSupportLevel.None)
-                            {
-                                lastPixel = pixel;
-                                console.Write(colors[pixel]);
-                                console.Write(colors[pixel]);
-                            }
-                            else if (pixel != lastPixel && colors.ContainsKey(pixel))
-                            {
-                                lastPixel = pixel;
-                                console.Write(colors[pixel]);
-                                console.Write(' ');
-                                console.Write(' ');
-                            }
-                            else
-                            {
-                                console.Write(' ');
-                                console.Write(' ');
-                            }
-                        }
-
-                        console.WriteLine();
-                    }
-
-                    if (showCounter == 1)
-                    {
-                        var message = string.Concat("You have nyaned for ", (int)(elapsed.Elapsed.TotalSeconds), " seconds!");
-                        var spacesLength = (width - message.Length) / 2;
-                        Span<char> spaces = stackalloc char[spacesLength];
-
-                        for (var i = 0; i < spacesLength; i++)
-                        {
-                            spaces[i] = ConsoleColorSupport.Level == ColorSupportLevel.None ? ',' : ' ';
-                        }
-
-                        console.Write(spaces);
-                        console.ColorBrightWhite();
-                        console.Write(message);
-                        console.Write(spaces);
-                    }
-
-                    console.WriteLine();
-                    console.Flush();
-
-                    lastPixel = char.MinValue;
-                    var sleep = (defaultSleep * 2) - watch.Elapsed.Milliseconds;
-                    Thread.Sleep(Math.Clamp(sleep, 0, defaultSleep));
-                    watch.Restart();
-                }
-            }
-
-            console.Dispose();
-            _shutdownBlock.Set();
-
-            return 0;
         }
 
-        private static Dictionary<char, string> GetColors()
+        private static long GetDefaultSleep()
         {
-            if (ConsoleColorSupport.Level.HasFlag(ColorSupportLevel.TrueColor))
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                return new Dictionary<char, string>
-                {
-                    { ','  , "\x1b[48;2;0;0;95m" },         /* Blue background */
-                    { '.'  , "\x1b[48;2;255;255;255m" },    /* White stars */
-                    { '\'' , "\x1b[48;2;0;0;0m" },          /* Black border */
-                    { '@'  , "\x1b[48;2;255;255;215m" },    /* Tan poptart */
-                    { '$'  , "\x1b[48;2;255;192;203m" },    /* Pink poptart */
-                    { '-'  , "\x1b[48;2;215;0;135m" },      /* Red poptart */
-                    { '>'  , "\x1b[48;2;255;0;0m" },        /* Red rainbow */
-                    { '&'  , "\x1b[48;2;255;165;0m" },      /* Orange rainbow */
-                    { '+'  , "\x1b[48;2;255;255;0m" },      /* Yellow Rainbow */
-                    { '#'  , "\x1b[48;2;135;255;0m" },      /* Green rainbow */
-                    { '='  , "\x1b[48;2;0;135;255m" },      /* Light blue rainbow */
-                    { ';'  , "\x1b[48;2;0;0;175m" },        /* Dark blue rainbow */
-                    { '*'  , "\x1b[48;2;88;88;88m" },       /* Gray cat face */
-                    { '%'  , "\x1b[48;2;215;135;175m" },    /* Pink cheeks */
-                };
-            }
-            else if (ConsoleColorSupport.Level.HasFlag(ColorSupportLevel.Ansi256))
-            {
-                return new Dictionary<char, string>
-                {
-                    { ','  , "\x1b[48;5;17m" },  /* Blue background */
-                    { '.'  , "\x1b[48;5;231m" }, /* White stars */
-                    { '\'' , "\x1b[48;5;16m" },  /* Black border */
-                    { '@'  , "\x1b[48;5;230m" }, /* Tan poptart */
-                    { '$'  , "\x1b[48;5;175m" }, /* Pink poptart */
-                    { '-'  , "\x1b[48;5;162m" }, /* Red poptart */
-                    { '>'  , "\x1b[48;5;196m" }, /* Red rainbow */
-                    { '&'  , "\x1b[48;5;214m" }, /* Orange rainbow */
-                    { '+'  , "\x1b[48;5;226m" }, /* Yellow Rainbow */
-                    { '#'  , "\x1b[48;5;118m" }, /* Green rainbow */
-                    { '='  , "\x1b[48;5;33m" },  /* Light blue rainbow */
-                    { ';'  , "\x1b[48;5;19m" },  /* Dark blue rainbow */
-                    { '*'  , "\x1b[48;5;240m" }, /* Gray cat face */
-                    { '%'  , "\x1b[48;5;175m" }, /* Pink cheeks */
-                };
-            }
-            else if (ConsoleColorSupport.Level.HasFlag(ColorSupportLevel.Basic))
-            {
-                return new Dictionary<char, string>
-                {
-                    { ',' , "\x1b[104m" }, /* Blue background */
-                    { '.' , "\x1b[107m" }, /* White stars */
-                    { '\'' , "\x1b[40m" }, /* Black border */
-                    { '@' , "\x1b[47m" },  /* Tan poptart */
-                    { '$' , "\x1b[105m" }, /* Pink poptart */
-                    { '-' , "\x1b[101m" }, /* Red poptart */
-                    { '>' , "\x1b[101m" }, /* Red rainbow */
-                    { '&' , "\x1b[43m" },  /* Orange rainbow */
-                    { '+' , "\x1b[103m" }, /* Yellow Rainbow */
-                    { '#' , "\x1b[102m" }, /* Green rainbow */
-                    { '=' , "\x1b[104m" }, /* Light blue rainbow */
-                    { ';' , "\x1b[44m" },  /* Dark blue rainbow */
-                    { '*' , "\x1b[100m" }, /* Gray cat face */
-                    { '%' , "\x1b[105m" }, /* Pink cheeks */
-                };
+                return 60;
             }
             else
             {
-                return new Dictionary<char, string>
-                {
-                    { ','  , "," }, /* Blue background */
-                    { '.'  , "." }, /* White stars */
-                    { '\'' , "'" }, /* Black border */
-                    { '@'  , "@" }, /* Tan poptart */
-                    { '$'  , "$" }, /* Pink poptart */
-                    { '-'  , "-" }, /* Red poptart */
-                    { '>'  , ">" }, /* Red rainbow */
-                    { '&'  , "&" }, /* Orange rainbow */
-                    { '+'  , "+" }, /* Yellow Rainbow */
-                    { '#'  , "#" }, /* Green rainbow */
-                    { '='  , "=" }, /* Light blue rainbow */
-                    { ';'  , ";" }, /* Dark blue rainbow */
-                    { '*'  , "*" }, /* Gray cat face */
-                    { '%'  , "%" }, /* Pink cheeks */
-                };
+                return 90;
             }
         }
 
-        private static int GetDefaultSleep()
-{
-    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-    {
-        return 60;
-    }
-    else
-    {
-        return 90;
-    }
-}
+        private static string GetVersion() => "v1.3.0";
 
-private static string GetVersion() => "v1.3.0";
-
-private static string GetName() => "nyancat";
+        private static string GetName() => "nyancat";
     }
 }
